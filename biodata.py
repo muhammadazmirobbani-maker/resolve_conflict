11f7080df33a06234e6739b9ad9910e9a7d7066f
MUHAMMAD <muhammadazmirobbani@smkitihsanulfikri.sch.id>
2026-01-29 08:28:21 +0700

    tambah filr biodata

---
+++ biodata.py
@@ -0,0 +1,13 @@
+print("=================================")
+print("Program Biodata Sederhana Dengan Python")
+print("=================================")
+
+nama = "azmi"
+hobi = "main bola"
+alamat = "cigelam"
+kota = "purwakarta"
+
+print("Nama Lengkap :", nama)
+print("Alamat       :", alamat, "\n Kota :", kota)
+print("Hobi         :", hobi)
+print("=================================")
